19f7448ab935b084efad15b3ed802ba9a37d6edb
Berna Fernandez <141880472+Speck763@users.noreply.github.com>
2023-09-13 16:42:30 -0300

    Update PruebaApplication.java

--- prueba/src/main/java/com/example/prueba/PruebaApplication.java
+++ prueba/src/main/java/com/example/prueba/PruebaApplication.java
@@ -2,14 +2,16 @@ package com.example.prueba;
 
 import com.example.prueba.entidades.Cliente;
 import com.example.prueba.entidades.Domicilio;
+import com.example.prueba.entidades.Pedido;
 import com.example.prueba.repositorios.ClienteRepository;
 import com.example.prueba.repositorios.DomicilioRepository;
+import com.example.prueba.repositorios.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service
 
 @SpringBootApplication
 public class PruebaApplication {
@@ -52,6 +54,16 @@ public class PruebaApplication {
 			// Guardar el cliente en la base de datos
 			clienteService.guardarCliente(cliente);
 
+			Pedido pedido = new Pedido();
+			pedido.setEstado(true);
+			pedido.setFecha(new Date());
+			pedido.setTipoEnvio(true);
+			pedido.setTotal(100.0);
+			pedido.setIniciado();
+			pedido.retiro(true);
+
+			pedido.mostrarEstado();
+			
 			// Recuperar el objeto Cliente desde la base de datos por ID
 			Cliente clienteRecuperado = clienteService.buscarClientePorId(cliente.getId());
 
@@ -62,6 +74,14 @@ public class PruebaApplication {
 				System.out.println("Email: " + clienteRecuperado.getEmail());
 				System.out.println("----------------------------------------");
 				clienteRecuperado.mostrarDomicilio();
+			}
+				if (pedidoRecuperado != null) {
+				System.out.println("Estado: " + pedidoRecuperado.isEstado());
+				System.out.println("Fecha: " + pedidoRecuperado.getFecha());
+				System.out.println("Tipo de Envío: " + pedidoRecuperado.isTipoEnvio());
+				System.out.println("Total: " + pedidoRecuperado.getTotal());
+				System.out.println("Estado Actual: " + pedidoRecuperado.getEstadoActual());
+				System.out.println("Envío: " + pedidoRecuperado.getEnvio());
 			}
 		};
 	}
