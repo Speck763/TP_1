d78dc694236470b6fbfafca8e6a562609b77c5be
Berna Fernandez <141880472+Speck763@users.noreply.github.com>
2023-09-18 12:21:32 -0300

    Update PruebaApplication.java

--- prueba/src/main/java/com/example/prueba/PruebaApplication.java
+++ prueba/src/main/java/com/example/prueba/PruebaApplication.java
@@ -16,115 +16,74 @@ public class PruebaApplication {
 
 	@Autowired
 	ClienteRepository clienteRepository;
+
 	@Autowired
 	DomicilioRepository domicilioRepository;
+
 	@Autowired
 	PedidoRepository pedidoRepository;
+
 	@Autowired
-	DetallePedidoRepository detallePedidoRepository;
-	@Autowired
-	ProductoRepository productoRepository;
-	@Autowired
-	RubroRepository rubroRepository;
+	FacturaRepository facturaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaApplication.class, args);
 	}
 
 	@Bean
-	CommandLineRunner init(ClienteRepository clienteRepo) {
+	CommandLineRunner init() {
 		return args -> {
 			System.out.println("-----------------ESTOY FUNCIONANDO---------");
 
-			Cliente cliente = Cliente.builder()
-					.nombre("Francisco")
-					.apellido("Saavedra")
-					.telefono("2611111111")
-					.email("abc@gmail.com")
-					.build();
+			// Crear instancias de Domicilio
+			Domicilio domicilio1 = new Domicilio();
+			domicilio1.setCalle("Calle 1");
+			domicilio1.setNumero("123");
 
-			Domicilio domicilio1 = Domicilio.builder()
-					.localidad("Guaymallen")
-					.calle("Carril Godoy Cruz")
-					.numero("800")
-					.build();
+			Domicilio domicilio2 = new Domicilio();
+			domicilio2.setCalle("Calle 2");
+			domicilio2.setNumero("456");
 
-			Domicilio domicilio2 = Domicilio.builder()
-					.localidad("Ciudad")
-					.calle("Av. San Martin")
-					.numero("1500")
-					.build();
+			// Crear instancia de Cliente
+			Cliente cliente = new Cliente();
+			cliente.setNombre("Juan");
+			cliente.setApellido("Pérez");
+			cliente.setTelefono("4850202");
+			cliente.setEmail("Juan_123@gmail.com");
 
-			Pedido pedido1 = Pedido.builder()
-					.estado(true)
-					.fecha(new Date())
-					.envio("delivery")
-					.total(2500)
-					.build();
+			// Agregar domicilios al cliente
+			cliente.agregarDomicilio(domicilio1);
+			cliente.agregarDomicilio(domicilio2);
 
-			Pedido pedido2 = Pedido.builder()
-					.estado(false)
-					.fecha(new Date())
-					.envio("delivery")
-					.total(1500)
-					.build();
-
-			DetallePedido dp1 = DetallePedido.builder()
-					.cantidad(1)
-					.subtotal(2000)
-					.build();
-
-			DetallePedido dp2 = DetallePedido.builder()
-					.cantidad(1)
-					.subtotal(500)
-					.build();
-
-			Producto producto1 = Producto.builder()
-					.tipo(true)
-					.tiempoEstimadoCocina(30)
-					.denominacion("Hamburguesa")
-					.precioVenta(2000)
-					.precioCompra(1000)
-					.stockActual(150)
-					.stockMinimo(50)
-					.unidadMedida("gr")
-					.receta("...")
-					.build();
-
-			Producto producto2 = Producto.builder()
-					.tipo(false)
-					.tiempoEstimadoCocina(0)
-					.denominacion("Coca-cola")
-					.precioVenta(500)
-					.precioCompra(200)
-					.stockActual(300)
-					.stockMinimo(10)
-					.unidadMedida("ml")
-					.receta("...")
-					.build();
-
-			Rubro rubro1 = Rubro.builder()
-					.denominacion("Alimentos procesados")
-					.build();
-
-			Rubro rubro2 = Rubro.builder()
-					.denominacion("Bebidas")
-					.build();
-
+			// Guardar el cliente en la base de datos
 			clienteRepository.save(cliente);
-//			domicilioRepository.save(domicilio1);
-//			domicilioRepository.save(domicilio2);
-//			pedidoRepository.save(pedido1);
-//			pedidoRepository.save(pedido2);
-//			detallePedidoRepository.save(dp1);
-//			detallePedidoRepository.save(dp2);
-//			productoRepository.save(producto1);
-//			productoRepository.save(producto2);
-			rubroRepository.save(rubro1);
-			rubroRepository.save(rubro2);
 
+//			// Crear una factura (opcional)
+//			Factura factura = new Factura();
+//			factura.setNumero(1);
+//			factura.setFecha(new Date());
+//			factura.setTotal(100);
+//			factura.setFormaPago(true);
+
+			// Asociar la factura al pedido (si es necesario)
+
+//			facturaRepository.save(factura);
+
+			// Crear un Pedido con o sin Factura
+			Pedido pedido = new Pedido();
+			pedido.setEstado(true);
+			pedido.setFecha(new Date());
+			pedido.setTipoEnvio(true);
+			pedido.setTotal(100);
+			pedido.setIniciado();
+			pedido.retiro(true);
+			pedido.setFactura(null);
+
+			// Guardar el pedido en la base de datos
+			pedidoRepository.save(pedido);
+
+			cliente.agregarPedidos(pedido);
 
 		};
 	}
 }
-
